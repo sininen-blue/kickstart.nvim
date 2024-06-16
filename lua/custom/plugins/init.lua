0eace363f66be5ade80c3c0555d4596c76efaa0d
sininen-blue <alaanneil@gmail.com>
2024-06-16 17:30:45 +0800

    added custom plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,27 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'folke/zen-mode.nvim',
+    opts = {
+      window = {
+        backdrop = 1, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
+        width = 85, -- width of the Zen window
+        height = 1, -- height of the Zen window
+        options = {
+          signcolumn = 'no', -- disable signcolumn
+          number = false, -- disable number column
+          relativenumber = false, -- disable relative numbers
+          cursorline = false, -- disable cursorline
+          cursorcolumn = false, -- disable cursor column
+          foldcolumn = '0', -- disable fold column
+          list = false, -- disable whitespace characters
+        },
+      },
+      on_open = function()
+        vim.cmd.colorscheme 'tokyonight-night'
+      end,
+    },
+  },
+}

d04e1811932f3586637e6fe6ca58837e8dd0f7f1
sininen-blue <108949710+sininen-blue@users.noreply.github.com>
2024-10-14 21:31:59 +0800

    added render markdown

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -39,4 +39,11 @@ return {
       require('nvim-highlight-colors').setup {}
     end,
   },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    opts = {},
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+  },
 }

5db748b110cd4f9ba35ba8aead066f3c85b7f252
sininenblue <alaanneil@gmail.com>
2026-03-02 23:29:07 +0800

    fully completed

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,13 +1,9 @@
 return {
-	"ellisonleao/gruvbox.nvim",
+	"rebelot/kanagawa.nvim",
 	priority = 1000,
 	config = function()
 		---@diagnostic disable-next-line: missing-fields
-		require("gruvbox").setup({
-			contrast = "soft",
-			overrides = {},
-		})
-		vim.o.background = "dark"
-		vim.cmd.colorscheme("gruvbox")
+		require("kanagawa").setup({})
+		vim.cmd.colorscheme("kanagawa")
 	end,
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -96,6 +96,7 @@ return {
 		local ensure_installed = vim.tbl_keys(servers or {})
 		vim.list_extend(ensure_installed, {
 			"ruff",
+			"python-lsp-server",
 		})
 
 		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })

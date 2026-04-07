999485de509f3a583cdbb4a7c96918c1857a5e38
Neil Alaan <alaanneil@gmail.com>
2026-04-07 11:33:51 +0800

    added settings

--- lua/custom/settings.lua
+++ lua/custom/settings.lua
@@ -7,6 +7,8 @@ vim.opt.autoindent = true
 vim.api.nvim_create_autocmd('FileType', {
   pattern = 'markdown',
   callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.linebreak = true
     vim.opt_local.spell = true
     vim.opt_local.spelllang = { 'en_us' }
   end,
